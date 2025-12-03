f7b0f03b82a789f3dff5a035682238ad03f298a3
aurelien <aurelien@kickersoft.com>
2025-12-03 17:33:48 -0500

    Add Footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css';
+
+export default function Footer({ children }) {
+    return (
+        <footer className={styles.footer}>
+            <p>© {new Date().getFullYear()} Aurélien.</p>
+            <p className={styles.links}>
+                <a href="https://github.com/aurelienec" target="_blank">GitHub</a>
+                <a href="https://www.linkedin.com/in/aurelien-antonio/" target="_blank">LinkedIn</a>
+            </p>
+        </footer>
+    );
+}
