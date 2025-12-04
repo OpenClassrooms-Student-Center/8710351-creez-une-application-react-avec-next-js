caf84f87654bf168fcb5043f6e342b46ccdc8da2
aurelien <aurelien@kickersoft.com>
2025-12-03 23:41:59 -0500

    Dynamisation du formulaire

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,6 +1,10 @@
+
+import ContactForm from './ContactForm/ContactForm'
 import styles from './page.module.css'
 
 export default function Contact() {
+
+
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Me Contacter</h1>
@@ -28,42 +32,7 @@ export default function Contact() {
                         </a>
                     </div>
                 </div>
-
-                <form className={styles.form}>
-                    <div className={styles.formGroup}>
-                        <label htmlFor="name">Nom</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            required
-                        />
-                    </div>
-
-                    <div className={styles.formGroup}>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            required
-                        />
-                    </div>
-
-                    <div className={styles.formGroup}>
-                        <label htmlFor="message">Message</label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            rows="5"
-                            required
-                        ></textarea>
-                    </div>
-
-                    <button type="submit" className={styles.submitBtn}>
-                        Envoyer le message
-                    </button>
-                </form>
+                <ContactForm />
             </div>
         </div>
     )
